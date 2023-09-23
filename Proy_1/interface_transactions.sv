b766f548ce58ebb5ca1123eb02db6e19328a7aee
Randallv07 <randallv07@gmail.com>
2023-09-22 22:09:31 -0600

    actualización del interface

--- Proy_1/interface_transactions.sv
+++ Proy_1/interface_transactions.sv
@@ -22,23 +22,54 @@ class trans_bus #(parameter pckg_sz = 32, parameter drvrs=5);
   constraint const_Rx {Rx < drvrs; Rx >= 0; Rx != Tx;}
   constraint const_Tx {Tx < drvrs; Tx >= 0}
 
-  function new(int ret =0,bit[pckg_sz-1:0] dto=0,int tmp = 0, tipo_trans tpo = lectura, int mx_rtrd = 10);
+  function new(int ret =0,bit[pckg_sz-1:0] dto=0,int tmp = 0, instruct tpo = cant_transac, int mx_rtrd = 20, tx, rx);
     this.retardo = ret;
     this.dato = dto;
     this.tiempo = tmp;
     this.tipo = tpo;
     this.max_retardo = mx_rtrd;
+    this.Tx = tx;
+    this.Rx = rx;
   endfunction
   
   function clean;
     this.retardo = 0;
     this.dato = 0;
     this.tiempo = 0;
-    this.tipo = lectura;
-    
+    this.tipo = cant_transac;
+    this.Tx = 0;
+    this.Rx = 0;
   endfunction
     
   function void print(string tag = "");
-    $display("[%g] %s Tiempo=%g Tipo=%s Retardo=%g dato=0x%h",$time,tag,tiempo,this.tipo,this.retardo,this.dato);
+    $display("[%g] %s Tiempo=%g Tipo=%s Retardo=%g Transmisor=0x%h dato=0x%h Receptor=0x%h",$time,tag,tiempo,this.tipo,this.retardo,this.Tx,this.dato,this.Rx);
   endfunction
+
 endclass
+
+////////////////////////////////////////////////////////////////
+// Interface: Esta es la interface que se conecta con el Bus  //
+////////////////////////////////////////////////////////////////
+
+interface bus_if #(parameter bits = 1,parameter drvrs = 4, parameter pckg_sz = 16, parameter broadcast = {8{1'b1}}) (
+  input bit clk
+);
+  logic reset;
+  logic pndng[bits-1:0][drvrs-1:0];
+  logic push[bits-1:0][drvrs-1:0];
+  logic pop[bits-1:0][drvrs-1:0];
+  logic [pckg_sz-1:0] D_pop[bits-1:0][drvrs-1:0];
+  logic [pckg_sz-1:0] D_push[bits-1:0][drvrs-1:0];
+endinterface
+
+////////////////////////////////////////////////////
+// Objeto de transacción usado en el scroreboard  //
+////////////////////////////////////////////////////
+
+//Aún no
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Definicion de mailboxes de tipo definido intruct para comunicar las interfaces //
+///////////////////////////////////////////////////////////////////////////////////////
+
+typedef mailbox #(instruct) comando_instrucciones_mbx;
