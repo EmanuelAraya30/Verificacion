8961b66bce68359742d73dedd4c16c09d8b66d37
Randallv07 <randallv07@gmail.com>
2023-10-05 14:00:42 -0600

    x

--- Proy_1/Respaldo final/test.sv
+++ Proy_1/Respaldo final/test.sv
@@ -3,8 +3,8 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
   
   
   //Definicion de mailboxes
-  test_agent_mailbox tam;
-  reporte_mailbox rm;
+  tam test_agent_mailbox;
+  rm reporte_mailbox;
   
   parameter num_trans_ag = 50;
   parameter max_retardo_ag = 30;
@@ -16,13 +16,13 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
   ambiente #(.pckg_sz(pckg_sz), .drvrs(drvrs), .bits(bits)) ambiente_inst;
   
   function new();
-    tam = new();
-    rm =new();
+  test_agent_mailbox = new();
+  reporte_mailbox =new();
     ambiente_inst = new();
     ambiente_inst.agent_inst.num_trans_agent = num_trans_ag;
     ambiente_inst.agent_inst.max_retardo_ag= max_retardo_ag;
-    ambiente_inst.agent_inst.tam=tam;
-    ambiente_inst.scoreboard_inst.rm =rm;
+    ambiente_inst.agent_inst.test_agent_mailbox=test_agent_mailbox;
+    ambiente_inst.scoreboard_inst.reporte_mailbox =reporte_mailbox;
   endfunction
   
   
@@ -34,23 +34,23 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
     
     
     tipo = trans_aleat; //cantidad de transacciones
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     $display("[%g] Transaccion aleatorizando numero de dispositivos", $time,num_trans_ag);
     
     tipo = broadcast; //Cantidad de dispositivos
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     $display("[%g] Transaccion aleatorizando numero de dispositivos", $time,num_trans_ag);
     
     tipo = trans_each; // Prueba de reset
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     $display("[%g] Transaccion con prueba de reset=%g", $time,num_trans_ag);
     
     tipo = trans_retarmin; // Retardo aleatorio 
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     $display("[%g] Transaccion con retardo aleatorio=%g", $time,num_trans_ag);
     
     tipo = trans_spec; // Direcciones de ID inválidas 
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     $display("[%g] Transacciones con direcciones de envio invalidas=%g", $time,num_trans_ag);
     
     
@@ -59,7 +59,7 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
     agent_inst.info_ag = 255;
     agent_inst.Tx_ag = drvrs-1;
     agent_inst.Rx_ag = 2;
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
 	
 	$display("[%g] Transacciones especificas creadas con n_transacciones=%g",$time,n_transacciones);
     #2000000;
@@ -74,11 +74,11 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
     ambiente_inst.agent_inst.num_trans_ag=100;
     ambiente_inst.agent_inst.max_retardo_ag=1;
     tipo=trans_aleat;
-	tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     #2000000;
       
     reporte_inst=reporte_bwmax;
-    rm.put(reporte_inst);
+    reporte_mailbox.put(reporte_inst);
     #100;
     ambiente_inst.scoreboard_inst.inicio=1;
     ambiente_inst.scoreboard_inst.cont_inst_bw=0;
@@ -91,15 +91,15 @@ class test #(parameter pckg_sz = 32, parameter drvrs =5, parameter bits=1);
     ambiente_inst.agent_inst.info_ag=0;
     ambiente_inst.agent_inst.Rx_ag=0;
     ambiente_inst.agent_inst.Tx_ag=drvrs-1;
-    tam.put(tipo);
+    test_agent_mailbox.put(tipo);
     #2000000;
       
     reporte_inst=reporte_bwmin;
-    rm.put(reporte_inst);
+    reporte_mailbox.put(reporte_inst);
     reporte_inst=reporte_promedio;
-    rm.put(reporte_inst);
+    reporte_mailbox.put(reporte_inst);
     reporte_inst=reporte_transacciones;
-    rm.put(reporte_inst);
+    reporte_mailbox.put(reporte_inst);
 	#100;
       	
     $display("[%g]numero de transacciones realizadas =%g", $time, ambiente_inst.scoreboard_inst.inst_count);
