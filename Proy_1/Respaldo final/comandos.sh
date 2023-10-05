548a1e0af7bfbbe98eb20167ec6cc6327314a676
Randallv07 <randallv07@gmail.com>
2023-10-05 14:24:08 -0600

    x

--- Proy_1/Respaldo final/comandos.sh
+++ Proy_1/Respaldo final/comandos.sh
@@ -6,11 +6,11 @@ for pckg_sz in  32
         for drvrs in 4 8 16
         do
         printf "\x60define PRMT \n" > parameters.sv
-        printf "parameter width = %d;\nparameter n_term = %d;\n" $width $n_term  >> parameters.sv
+        printf "parameter pckg_sz = %d;\nparameter drvrs = %d;\n" $pckg_sz $drvrs  >> parameters.sv
         vcs -Mupdate parameters.sv testbench.sv  -o salida  -full64 -sverilog  -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test +lint=TFIPC-L
         ./salida +ntb_random_seed=20
         if $?; then
-        printf "\n\n\n\n\nError in run n_term=%d, width=%d\n\n\n\n" $width $n_term
+        printf "\n\n\n\n\nError in run drvrs=%d, pckg_sz=%d\n\n\n\n" $pckg_sz $drvrs
                                 return 1
                         fi
         done
