f72f15d68a23c1396bb94117cda60e8787807c12
Randallv07 <randallv07@gmail.com>
2023-10-06 17:05:57 -0600

    Checker y ambiente

--- Proy_1/Respaldo/checker.sv
+++ Proy_1/Respaldo/checker.sv
@@ -12,18 +12,18 @@
 // según su especificación
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Checker/scoreboard: este objeto es responsable de verificar que el comportamiento del DUT sea el esperado //
+// Checker: este objeto es responsable de verificar que el comportamiento del DUT sea el esperado //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 class checker #(parameter pckg_sz = 32, parameter bits=1, parameter drvrs=5);
-  trans_bus #(.pckg_sz(pckg_sz), .drvrs(drvrs)) transaccion; //transacción recibida en el mailbox 
-  trans_bus #(.pckg_sz(pckg_sz), .drvrs(drvrs)) emul_queue[drvrs][$]; //this queue is going to be used as golden reference for the fifo
-  trans_monitor #(.pckg_sz(pckg_sz)) mntr_trans;
+  trans_bus #(.pckg_sz(pckg_sz), .drvrs(drvrs)) transaccion; //transacción del agente
+  trans_bus #(.pckg_sz(pckg_sz), .drvrs(drvrs)) emul_queue[drvrs][$]; //Esta cola va a se usada como golden reference para el bus
+  trans_monitor #(.pckg_sz(pckg_sz)) mntr_trans; //Transacción del monitor
   trans_sb   #(.pckg_sz(pckg_sz)) to_sb; // transacción usada para enviar al checker desde el monitor
 
   //Llamado de mailboxes
-  dcm driver_checker_mailbox; 
-  mcm monitor_checker_mailbox; // Este mailbox es el que comunica con el monitor con el checker
-  csm checker_scoreboard_mailbox; // Este mailbox es el que comunica el checker con el scoreboard
+  dcm driver_checker_mailbox; // mailbox es el que comunica con el driver con el checker
+  mcm monitor_checker_mailbox; // mailbox es el que comunica con el monitor con el checker
+  csm checker_scoreboard_mailbox; // mailbox es el que comunica el checker con el scoreboard
 
   //Se generan Queues para almacenar datos
   function new();
@@ -32,7 +32,8 @@ class checker #(parameter pckg_sz = 32, parameter bits=1, parameter drvrs=5);
     end
   endfunction 
 
-  task save;
+  //Se inicializa el checker y se guardan las transacciones del agente
+  task guardar;
     $display("[%g]  El checker fue inicializado",$time);
     forever begin
       driver_checker_mailbox.get(transaccion);
@@ -41,14 +42,15 @@ class checker #(parameter pckg_sz = 32, parameter bits=1, parameter drvrs=5);
     end 
   endtask
 
-  task match; //Compara los datos enviados contra los recibidos
+  //Compara datos enviados por agente contra los recibidos por el monitor
+  task comparar; 
     forever begin
-      to_sb = new();
+      to_sb = new(); //Transacciones de Scoreboard
       monitor_checker_mailbox.get(mntr_trans);
       $display("[%g]  Checker: Se recibe trasacción desde el monitor",$time);
-      for(int i=0; i<emul_queue[mntr_trans.dato_rec_mnt].size(); i++)begin //Recorre cada posicion
-        if(emul_queue[mntr_trans.dato_rec_mnt][i].dato==mntr_trans.dato)begin //compara
-          to_sb.dato_env=emul_queue[mntr_trans.dato_rec_mnt][i].dato;
+      for(int i=0; i<emul_queue[mntr_trans.dato_rec_mnt].size(); i++)begin // Recorre las transacciones
+        if(emul_queue[mntr_trans.dato_rec_mnt][i].dato==mntr_trans.dato)begin // Las compara con lo que se obtiene en monitor
+          to_sb.dato_env=emul_queue[mntr_trans.dato_rec_mnt][i].dato; 
           to_sb.dato_rec=mntr_trans.dato;
           to_sb.tiempo_env=emul_queue[mntr_trans.dato_rec_mnt][i].tiempo;
           to_sb.tiempo_rec=mntr_trans.tiempo;
@@ -56,11 +58,18 @@ class checker #(parameter pckg_sz = 32, parameter bits=1, parameter drvrs=5);
           to_sb.tipo=emul_queue[mntr_trans.dato_rec_mnt][i].tipo;
           to_sb.dato_env=emul_queue[mntr_trans.dato_rec_mnt][i].dato_env;
           to_sb.dev_rec=mntr_trans.dato_rec_mnt;
+          $display("[%g]  ******************************************************",$time);
           to_sb.print("Checker: Transacción completa");
-          checker_scoreboard_mailbox.put(to_sb);//envia la transaccion al scoreboard
+          $display("[%g]  ******************************************************",$time);
+          checker_scoreboard_mailbox.put(to_sb);  // Guarda los resultados y los envia en el mailbox
           i=emul_queue[mntr_trans.dato_rec_mnt].size();  	
         end
-        
+        else begin
+          $display("[%g]  ******************************************************",$time);
+          $display("[%g]  Checker: Error, los datos son diferentes a lo esperado",$time);
+          to_sb.print("Checker: Transacción incompleta");
+          $display("[%g]  ******************************************************",$time);
+        end
       end
     end
 
